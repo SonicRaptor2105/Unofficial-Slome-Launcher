452aaaa8e683e93a8afc73a6c3806313d967b186
SonicRaptor2105 <sonicraptor2105@gmail.com>
2025-04-04 23:09:17 +1100

    Started on colour changing Slome

--- Unofficial Slome Launcher.py
+++ Unofficial Slome Launcher.py
@@ -14,11 +14,15 @@ versionList = os.listdir('versions')
 scroll = 0
 versions = []
 closeLauncher = True
-playerAppearance = []
+profile = []
 
 button = pygame.image.load('launcher/button.png')
 textFont = pygame.font.SysFont(None, 40)
 smallTextFont = pygame.font.SysFont(None, 30)
+
+windowsSlomePath = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER,r"Software\ZeroEightStudios\Slome")
+profile = [winreg.EnumValue(windowsSlomePath, 25)[1], (winreg.EnumValue(windowsSlomePath, 26)[1], winreg.EnumValue(windowsSlomePath, 27)[1], winreg.EnumValue(windowsSlomePath, 28)[1])]
+
 def drawButton(text, font, textColour, x, y):
     screen.blit(button, [x, y])
     displayName = font.render(text, True, textColour)
@@ -30,25 +34,44 @@ def error(message):
     pygame.display.update()
     sleep(0.8)
     return
-
+    
 
 
 running = True
 while running:
     screen.fill('white')
     screen.blit(pygame.image.load('launcher/leftSide.png'), (0,0))
-    #The following two assests are taken directly from Slome and edited. They are not owned or created by me
     screen.blit(pygame.image.load('launcher/rightSide.png'), (400,0))
-    screen.blit(pygame.image.load('launcher/slomePlaceholder.png'), (75,80))
 
-    screen.blit((smallTextFont.render('a0.1.0', True, (255,255,255))), (14, 690))
+    profile[0] = profile[0]
+    screen.blit((smallTextFont.render(profile[0], True, (255, 255, 255))), (70, 50))
+    sprite = pygame.image.load('launcher/slomePlaceholder.png').convert()
+    if profile[1] == (0,0,0):
+        profile[1] = (1,1,1)
+    sprite.set_colorkey((0,0,0))
+    pixels = pygame.PixelArray(sprite)
+    pixels.replace((255,255,255), profile[1])
+    temp = list(profile[1])
+    x = 0
+    while x < 3:
+        if profile[1][x] - 55 >= 0:
+            temp[x] = profile[1][x] - 55
+        else:
+            temp[x] = 1
+        x+=1
+    pixels.replace((200,200,200), (temp[0], temp[1], temp[2]))
+    pixels.close()
+    del(temp)
+    screen.blit(sprite, (72,80))
+
+
+    screen.blit((smallTextFont.render('a0.1.1', True, (255,255,255))), (14, 690))
 
     if closeLauncher == True:
-        screen.blit((smallTextFont.render('Keep launcher open', True, (255,255,255))), (50, 402))
-        pygame.draw.lines(screen, [255,255,255], True, [(20,420), (40,420), (40,400), (20,400)])
+        pygame.draw.lines(screen, [255,255,255], True, [(20,550), (40,550), (40,570), (20,570)])
     else:
-        screen.blit((smallTextFont.render('Keep launcher open', True, (255,255,255))), (50, 402))
-        pygame.draw.rect(screen, [255,255,255], [20, 400, 20, 20])
+        pygame.draw.rect(screen, [255,255,255], [20, 550, 20, 20])
+    screen.blit((smallTextFont.render('Keep launcher open', True, (255,255,255))), (50, 552))
 
     x = 0
     while x < len(versionList):
@@ -88,23 +111,10 @@ while running:
                                 except:
                                     error('No Slome.exe file found at path')
                     x+=1
-                if 20 <= mousePosition[0] <= 40 and 400 <= mousePosition[1] <= 420:
+                if 20 <= mousePosition[0] <= 40 and 550 <= mousePosition[1] <= 570:
                     closeLauncher = not closeLauncher
         elif event.type == pygame.MOUSEWHEEL:
             if (scroll >= 0 and event.y > 0) or (versions[-1][3] <= 660 and event.y < 0):
-                windowsSlomePath = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER,r"Software\ZeroEightStudios\Slome")
-                playerAppearance = [winreg.EnumValue(windowsSlomePath, 25)[1], winreg.EnumValue(windowsSlomePath, 26)[1], winreg.EnumValue(windowsSlomePath, 27)[1], winreg.EnumValue(windowsSlomePath, 28)[1]]
-                print(playerAppearance)
-#prints out all files in Slome windows registory
-                '''
-                x=0
-                while True:
-                    try:
-                        print(x, ': ', winreg.EnumValue(windowsSlomePath, x))
-                        x+=1
-                    except:
-                        break
-                '''
                 pass
             else:
                 scroll += event.y * 15
@@ -113,5 +123,3 @@ while running:
     pygame.display.update()
 
 pygame.quit()
-
-#C:\Users\mjham\AppData\LocalLow\ZeroEightStudios\Slome
